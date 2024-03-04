56c9828d99da7890a0757f8262339bd4cde91e87
martin0626 <scbsteel12@gmail.com>
2024-03-04 19:40:02 +0200

    Added More error handlers

--- controllers/errorControllers.js
+++ controllers/errorControllers.js
@@ -1,3 +1,29 @@
+const appError = require('./../utils/appError');
+
+
+const handleCastErr = (err)=>{
+    const message = `Inavlid ${err.path}: ${err.value}`
+    return new appError(message, 400);
+}
+
+
+const handleDublicateFieldsDB = err =>{
+    let value = Object.values(err.keyValue)[0];
+    let field = Object.keys(err.keyValue)[0];
+    
+    const message = `Duplicate field value: ${value} for ${field}. Please use another value.`;
+    return new appError(message, 400);
+}
+
+
+const handleValidationErrorDB = err =>{
+    const errors = Object.values(err.errors).map(el => el.message);
+    const message = `Invalid input data: ${errors.join('. ')}`;
+
+    return new appError(message, 400);
+}
+
+
 const sendErrorDev = (err, res) =>{
     res.status(err.statusCode).json({
         status: err.status,
@@ -33,6 +59,12 @@ module.exports = (err, req, res, next)=>{
     if(process.env.NODE_ENV === 'development'){
         sendErrorDev(err, res);
     }else if(process.env.NODE_ENV === 'production'){
-        sendErrorProd(err, res);
+
+        let error = {...err};
+        if(err.name == 'CastError') error = handleCastErr(error);
+        if(error.code == 11000) error = handleDublicateFieldsDB(error);
+        if(err.name == 'ValidationError') error = handleValidationErrorDB(error);
+
+        sendErrorProd(error, res);
     }
 };
--- server.js
+++ server.js
@@ -9,6 +9,16 @@ dotenv.config({path: './config.env'})
 const app = require('./app');
 const port = process.env.PORT || 3000;
 
-app.listen(port, ()=>{
+const server = app.listen(port, ()=>{
     console.log("App runing on port " + port + " ...");
+})
+
+process.on('unhandledRejection', err=>{
+    console.log(err.name, err.message);
+    console.log('UNHANDLED REJECTION! Shutting down...');
+
+    server.close(()=>{
+        process.exit(1);
+    })
+
 })
